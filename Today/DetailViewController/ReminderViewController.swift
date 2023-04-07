31ee46e7c85f7d06ea78c0305fa4d04f9cb2456d
Felipe Christian Lautenschlager <felipelauten@gmail.com>
2023-04-08 00:12:33 +0200

    Displaying reminder details + styling the app

---
+++ Today/DetailViewController/ReminderViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ReminderViewController.swift
+//  Today
+//
+//  Created by Felipe Christian Lautenschlager on 07/04/2023.
+//
+
+import UIKit
+
+class ReminderViewController: UICollectionViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, Row>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Row>
+
+    var reminder: Reminder
+    private var dataSource: DataSource!
+
+    init(reminder: Reminder) {
+        self.reminder = reminder
+
+        var listConfiguration = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+        listConfiguration.showsSeparators = false
+
+        let listLayout = UICollectionViewCompositionalLayout.list(using: listConfiguration)
+
+        super.init(collectionViewLayout: listLayout)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("Always initialize ReminderViewController using init(reminder:)")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let cellRegistration = UICollectionView.CellRegistration(handler: cellRegistrationHandler)
+        dataSource = DataSource(collectionView: collectionView, cellProvider: {
+            (collectionView: UICollectionView, indexPath: IndexPath, itemIdentifier: Row) in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        })
+
+        if #available(iOS 16, *) {
+            navigationItem.style = .navigator
+        }
+        navigationItem.title = NSLocalizedString("Reminder", comment: "Reminder view controller title")
+        updateSnapshot()
+    }
+
+    func cellRegistrationHandler(cell: UICollectionViewListCell, indexPath: IndexPath, row: Row) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = text(for: row)
+        contentConfiguration.textProperties.font = UIFont.preferredFont(forTextStyle: row.textStyle)
+        contentConfiguration.image = row.image
+        cell.contentConfiguration = contentConfiguration
+        cell.tintColor = .todayPrimaryTint
+    }
+
+    func text(for row: Row) -> String? {
+        switch row {
+        case .date: return reminder.dueDate.dayText
+        case .notes: return reminder.notes
+        case .title: return reminder.title
+        case .time: return reminder.dueDate.formatted(date: .omitted, time: .shortened)
+        }
+    }
+
+    func updateSnapshot() {
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems([Row.title, Row.date, Row.time, Row.notes], toSection: 0)
+        dataSource.apply(snapshot)
+    }
+
+}
